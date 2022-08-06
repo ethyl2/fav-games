9d91fd050f630cfb82e00b742e7c888c5e92b29c
ethyl2 <zzzuzz@gmail.com>
2022-08-06 15:36:07 -0600

    puts game info in its own js file

--- index.js
+++ index.js
@@ -72,29 +72,6 @@ shareButton.addEventListener('click', (event) => {
     }
 });
 
-const games = [
-    {
-        title: 'Codenames',
-        url: 'https://codenames.game/',
-        gameLogoSrc: 'images/Codenames.png',
-        gameLogoAlt: 'Codenames art',
-        description: 'The Top Secret Word Game - Win or lose, it\'s fun to figure out the clues!',
-        howTo: `<p class="text-xs mt-2 md:text-sm"> A fun and challenging social word game with a simple premise. Two rival spymasters know the secret
-        identities of 25 agents. Their teammates know the agents only by their CODENAMES. Teams compete to contact all of their agents first.
-        </p>`,
-    },
-    {
-        title: 'Skribbl.io',
-        url: 'https://skribbl.io/',
-        gameLogoSrc: 'images/Skribblio.jpg',
-        gameLogoAlt: 'Skribbl.io logo',
-        description: 'Free multiplayer drawing and guessing game',
-        howTo: `<p class="text-xs mt-2 md:text-sm"> One game consists of a few rounds in which every round someone has to draw their chosen word and others have to guess it to gain points!
-                  The person with the most points at the end of game will then be crowned as the winner!
-              </p>`,
-    }
-]
-
 const testEl = document.getElementById('test')
 games.forEach(game => {
     const gameLi = document.createElement('li')
