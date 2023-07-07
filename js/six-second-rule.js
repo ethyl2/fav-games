ee5a2e6e65f643c1fb983a8681f6bae12a00aaf3
Heather Nuffer <hnuffer@bestcompany.com>
2023-07-06 19:23:28 -0600

    Adds timer reset

--- js/six-second-rule.js
+++ js/six-second-rule.js
@@ -16,6 +16,9 @@ const progressBar = document.getElementById('progress-bar')
 const countdown = document.getElementById('countdown')
 countdown.textContent = `${(timeLeft/2).toFixed(1)} seconds remaining`
 const timerStartButton = document.getElementById('timer-start')
+const timerResetButton = document.getElementById('timer-reset')
+let stopTimer = false
+
 const audio = new Audio('static/audio/guitar-riff.wav')
 const audio2 = new Audio('static/audio/times-up.wav')
 const audio3 = new Audio('static/audio/tada.wav')
@@ -28,7 +31,24 @@ const pointsEl = document.getElementById('points')
 let points = 0
 const pointsResetButton = document.getElementById('points-reset')
 
+timerResetButton.addEventListener('click', () => {
+  stopTimer = true
+  timerStartButton.textContent = 'Start Timer'
+  timerStartButton.disabled = false
+  timerStartButton.classList.add('hover:bg-blue-400')
+  timerStartButton.classList.remove('cursor-not-allowed')
+
+  timeLeft = maxTime
+
+  timerResetButton.disabled = true
+  timerResetButton.classList.add('cursor-not-allowed', 'opacity-50')
+})
+
 timerStartButton.addEventListener('click', () => {
+  stopTimer = false
+  timerResetButton.disabled = false
+  timerResetButton.classList.remove('cursor-not-allowed', 'opacity-50')
+
   timerStartButton.textContent = 'Hurry! Hurry!'
   timerStartButton.disabled = true
   timerStartButton.classList.remove('hover:bg-blue-400')
@@ -41,40 +61,47 @@ timerStartButton.addEventListener('click', () => {
   }
 
   const downloadTimer = setInterval(function(){
-  if(timeLeft <= 0){
-    clearInterval(downloadTimer);
-    mainContainer.classList.remove('bg-white')
-    mainContainer.classList.add('bg-yellow-200')
-    setTimeout(() => {
-        mainContainer.classList.remove('bg-yellow-200')
-        mainContainer.classList.add('bg-white')
-    }, 500)
-    setTimeout(() => {
-        mainContainer.classList.remove('bg-white')
-        mainContainer.classList.add('bg-yellow-100')
-    }, 1000)
-    setTimeout(() => {
-        mainContainer.classList.remove('bg-yellow-100')
-        mainContainer.classList.add('bg-white')
-    }, 1500)
-
-    countdown.textContent = "Time's up!"
-    timeLeft -= 1
-    progressBar.value = maxTime - timeLeft
-    timerStartButton.textContent = 'Start Timer'
-    timerStartButton.disabled = false
-    timerStartButton.classList.add('hover:bg-blue-400')
-    timerStartButton.classList.remove('cursor-not-allowed')
-    if (allowAudio) {
-      audio2.play()
+    if (stopTimer) {
+      clearInterval(downloadTimer)
     }
-  }
-  else {
-    progressBar.value = maxTime - timeLeft
-    countdown.textContent = `${(timeLeft/2).toFixed(1)} seconds remaining`
-    timeLeft -= 1
-  }
-}, 500);
+    if(timeLeft <= 0){
+      clearInterval(downloadTimer);
+      mainContainer.classList.remove('bg-white')
+      mainContainer.classList.add('bg-yellow-200')
+      setTimeout(() => {
+          mainContainer.classList.remove('bg-yellow-200')
+          mainContainer.classList.add('bg-white')
+      }, 500)
+      setTimeout(() => {
+          mainContainer.classList.remove('bg-white')
+          mainContainer.classList.add('bg-yellow-100')
+      }, 1000)
+      setTimeout(() => {
+          mainContainer.classList.remove('bg-yellow-100')
+          mainContainer.classList.add('bg-white')
+      }, 1500)
+
+      countdown.textContent = "Time's up!"
+      timeLeft -= 1
+      progressBar.value = maxTime - timeLeft
+      timerStartButton.textContent = 'Start Timer'
+      timerStartButton.disabled = false
+      timerStartButton.classList.add('hover:bg-blue-400')
+      timerStartButton.classList.remove('cursor-not-allowed')
+
+      timerResetButton.disabled = true
+      timerResetButton.classList.add('cursor-not-allowed', 'opacity-50')
+      
+      if (allowAudio) {
+        audio2.play()
+      }
+    }
+    else {
+      progressBar.value = maxTime - timeLeft
+      countdown.textContent = `${(timeLeft/2).toFixed(1)} seconds remaining`
+      timeLeft -= 1
+    }
+  }, 500);
 })
 
 window.addEventListener('load', () => {
